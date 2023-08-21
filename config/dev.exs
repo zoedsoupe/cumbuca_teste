b0d3238cfedac37dfd6d8d7bde60382ae6206725
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-08-21 15:56:24 -0300

    feat: create read replica for ecto repository

--- config/dev.exs
+++ config/dev.exs
@@ -1,13 +1,17 @@
 import Config
 
-# Configure your database
-config :cumbuca, Cumbuca.Repo,
+repo_opts = [
   username: "zoedsoupe",
   hostname: "localhost",
   database: "cumbuca_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
+]
+
+# Configure your database
+config :cumbuca, Cumbuca.Repo, repo_opts
+config :cumbuca, Cumbuca.Repo.Replica, [{:default_dynamic_repo, Cumbuca.Repo.Replica} | repo_opts]
 
 config :cumbuca, CumbucaWeb.Endpoint,
   http: [ip: {0, 0, 0, 0}, port: 4000],
--- lib/cumbuca/repo.ex
+++ lib/cumbuca/repo.ex
@@ -3,3 +3,12 @@ defmodule Cumbuca.Repo do
     otp_app: :cumbuca,
     adapter: Ecto.Adapters.Postgres
 end
+
+defmodule Cumbuca.Repo.Replica do
+  use Ecto.Repo,
+    otp_app: :cumbuca,
+    adapter: Ecto.Adapters.Postgres,
+    read_only: true,
+    default_dynamic_repo:
+      Application.compile_env!(:cumbuca, [Cumbuca.Repo.Replica, :default_dynamic_repo])
+end
