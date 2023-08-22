fdea5ca0c68c6e9ddd9f58e81f03c9f83bf0e880
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-08-21 23:29:40 -0300

    feat: add semantic macros for context's usage

--- lib/cumbuca.ex
+++ lib/cumbuca.ex
@@ -1,3 +1,40 @@
 defmodule Cumbuca do
   @moduledoc false
+
+  def model do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      alias __MODULE__
+
+      @opaque changeset :: Ecto.Changeset.t()
+      @callback changeset(__MODULE__.t(), map) :: changeset
+      @behaviour __MODULE__
+    end
+  end
+
+  def schema do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      alias __MODULE__
+
+      @callback parse!(map) :: __MODULE__.t()
+      @behaviour __MODULE__
+    end
+  end
+
+  def repository do
+    quote do
+      import Ecto.Query
+      alias Cumbuca.Repo
+      alias Cumbuca.Repo.Replica
+
+      def delete(entity), do: Repo.delete(entity)
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
