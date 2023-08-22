31b049411db0f0347b8dbd2530de6d7a9cd9f23a
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-08-21 23:30:43 -0300

    feat: create user migration and add wrapper functions to repo

--- lib/cumbuca/repo.ex
+++ lib/cumbuca/repo.ex
@@ -2,4 +2,16 @@ defmodule Cumbuca.Repo do
   use Ecto.Repo,
     otp_app: :cumbuca,
     adapter: Ecto.Adapters.Postgres
+
+  @opaque id :: integer
+  @opaque changeset :: Ecto.Changeset.t()
+
+  @spec fetch_by(module, keyword) :: {:ok, struct} | {:error, :not_found}
+  def fetch_by(model, params) do
+    if result = get_by(model, params) do
+      {:ok, result}
+    else
+      {:error, :not_found}
+    end
+  end
 end
---
+++ priv/repo/migrations/20230821193311_create_user_table.exs
@@ -0,0 +1,16 @@
+defmodule Cumbuca.Repo.Migrations.CreateUserTable do
+  use Ecto.Migration
+
+  def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
+
+    create table(:user) do
+      add :cpf, :citext, null: false
+      add :first_name, :string, null: false
+      add :last_name, :string
+      add :public_id, :string, null: false
+
+      timestamps()
+    end
+  end
+end
