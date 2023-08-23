b537c533ff97fe4ccfdd7093d858fe8932333239
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-08-23 12:38:02 -0300

    feat: split main workflow into small pieces

--- lib/cumbuca/transactions.ex
+++ lib/cumbuca/transactions.ex
@@ -13,37 +13,79 @@ defmodule Cumbuca.Transactions do
 
   @spec schedule_new_transaction(transact_params) :: :ok
   def schedule_new_transaction(params) do
-    with {:ok, transaction} <-
-           params
-           |> TransactionAdapter.external_to_internal()
-           |> Repository.upsert_transaction() do
-      Logger.info("[#{__MODULE__}] ==> Transaction scheduled: #{transaction.identifier}")
+    with {:ok, struct} <- TransactionAdapter.external_to_internal(params),
+         {:ok, transaction} <- Repository.upsert_transaction(Map.from_struct(struct)) do
+      transaction.identifier
+      |> transaction_process_scheduled_message()
+      |> Logger.info()
     end
   end
 
   def transact!(%TransactEvent{} = event) do
-    with {:ok, _transaction} <- update_transsaction_status(event.transaction_identifier),
-         {:ok, sender} <- Accounts.retrieve_bank_account(event.sender_identifier),
+    with {:ok, sender} <- Accounts.retrieve_bank_account(event.sender_identifier),
          {:ok, receiver} <- Accounts.retrieve_bank_account(event.receiver_identifier),
-         :ok <- TransactionLogic.validate_transaction(sender, receiver, event.amount) do
-      {:ok, :done} = Accounts.transfer_amount_between_accounts(sender, receiver, event.amount)
+         :ok <- maybe_fails_transaction(sender, receiver, event),
+         {:ok, :done} <-
+           Accounts.transfer_amount_between_accounts(sender, receiver, event.amount),
+         {:ok, _transaction} <- set_transaction_processed(event) do
       :ok
     else
+      :error ->
+        event
+        |> transfer_error_message()
+        |> Logger.error()
+
       {:error, :not_found} ->
-        Logger.error(
-          "[#{__MODULE__}] ==> Transaction #{inspect(event)} failed because one of accounts does not exist"
-        )
+        event
+        |> account_does_not_exists_error_message()
+        |> Logger.error()
 
       {:error, validation_error} ->
-        Logger.error(
-          "[#{__MODULE__}] ==> Transaction #{inspect(event)} failed with: #{validation_error}"
-        )
+        event
+        |> transaction_validation_error_message(validation_error)
+        |> Logger.error()
     end
   end
 
-  defp update_transsaction_status(identifer) do
-    with {:ok, transaction} <- Repository.fetch_transaction(identifer) do
-      Repository.upsert_transaction(transaction, %{processed_at: NaiveDateTime.utc_now()})
+  defp maybe_fails_transaction(sender, receiver, event) do
+    case TransactionLogic.validate_transaction(sender, receiver, event.amount) do
+      {:error, _} = err ->
+        set_transaction_failed(event)
+        err
+
+      :ok ->
+        :ok
     end
+  end
+
+  defp set_transaction_failed(%TransactEvent{} = event) do
+    with {:ok, transaction} <- Repository.fetch_transaction(event.transaction_identifier) do
+      Repository.upsert_transaction(transaction, %{status: :failed})
+    end
+  end
+
+  defp set_transaction_processed(%TransactEvent{} = event) do
+    with {:ok, transaction} <- Repository.fetch_transaction(event.transaction_identifier) do
+      Repository.upsert_transaction(transaction, %{
+        status: :success,
+        processed_at: NaiveDateTime.utc_now()
+      })
+    end
+  end
+
+  defp transaction_validation_error_message(event, error) do
+    "[#{__MODULE__}] ==> Transaction #{inspect(event)} failed with: #{error}"
+  end
+
+  defp account_does_not_exists_error_message(event) do
+    "[#{__MODULE__}] ==> Transaction #{inspect(event)} failed because one of accounts does not exist"
+  end
+
+  defp transaction_process_scheduled_message(identifier) do
+    "[#{__MODULE__}] ==> Transaction scheduled: #{identifier}"
+  end
+
+  defp transfer_error_message(event) do
+    "[#{__MODULE__}] ==> Transaction #{inspect(event)} failed transfer between accounts"
   end
 end
